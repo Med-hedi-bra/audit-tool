6e3088d45a813f192a28bccdbdfd3dae2d46fc2f
Bra <mohamedhedibra8@gmail.com>
2024-03-20 12:07:53 +0100

    adding the feature to validate the authorization based on role with a token based authentication

--- testing_auth/services/AuthTestingService.py
+++ testing_auth/services/AuthTestingService.py
@@ -1,4 +1,5 @@
 from io import StringIO
+import json
 import re
 from urllib.parse import urljoin
 import requests
@@ -46,16 +47,105 @@ class AuthTestingService:
 
         return results
 
- 
     @staticmethod
     def verify_username_strength(username):
-        pattern = r'^[a-zA-Z0-9_]{3,20}$'  
+        pattern = r"^[a-zA-Z0-9_]{3,20}$"
         regex = re.compile(pattern)
 
         if regex.match(username):
             return True
         else:
             return False
-        
 
-                
+    @staticmethod
+    def get_nested_token(json_data: str, keys_as_string: str):
+        # exaample of keys_as_string: "user.info.auth"
+        keys = keys_as_string.split(".")
+        data = json.loads(json_data)
+        for key in keys:
+            if isinstance(data, dict) and key in data:
+                data = data[key]
+            else:
+                return None
+        return data.get("token", None)
+
+    @staticmethod
+    def get_token_by_login(
+        website_url,username: str, password: str, auth_uri: str, token_path: str
+    ):
+        # exaample of token_path_in_response: "user.info.auth"
+        
+        login_data = {
+            'username': username,
+            'password': password
+        }
+        # Make a request to obtain the token
+        url = urljoin(website_url, auth_uri)
+        response = requests.post(url=url, json=login_data)
+        response_data = response.json()
+       
+        token = AuthTestingService.get_nested_token(
+            json.dumps(response_data), token_path)
+        
+        if token:
+            return token
+        else:
+            raise Exception("Failed to obtain access token")
+
+
+    @staticmethod
+    def verify_access_to_one_ressource(
+        website_url, role_username ,role_password, auth_uri, token_path,http_method, http_response_code_for_access_allowed, http_response_code_for_access_denied
+    ):
+        """
+        Verify if the user can access to the ressources
+        """
+        # Get the token
+        try:
+            token = AuthTestingService.get_token_by_login(username=role_username, password=role_password, website_url=website_url, auth_uri=auth_uri, token_path=token_path)
+            headers = {'Authorization': f'Bearer {token}'}
+            response = requests.request(method=http_method, url=website_url, headers=headers)
+
+            if response.status_code == http_response_code_for_access_allowed:
+                return True
+            elif response.status_code == http_response_code_for_access_denied:
+                return False
+            else:
+                return False
+        except Exception as e:
+            return False
+
+    @staticmethod
+    def check_role_authorization(
+        website_url, services, all_roles, auth_uri, token_path
+    ):
+        """
+        Check if the roles are authorized to access the services
+        """
+        ans=[]
+        # Check if the roles are authorized to access the services
+        for service in services:
+            # i should pass them as an argument
+            http_method = service["http_method"]
+            http_response_code_for_access_allowed = service["http_response_code_for_access_allowed"]
+            http_response_code_for_access_denied = service["http_response_code_for_access_denied"]
+            
+            
+            for role in all_roles:
+                has_access = AuthTestingService.verify_access_to_one_ressource(
+                    website_url=website_url,
+                    role_username=role["username"],
+                    role_password=role["password"],
+                    auth_uri=auth_uri,
+                    token_path = token_path,
+                    http_method=http_method,
+                    http_response_code_for_access_allowed=http_response_code_for_access_allowed,
+                    http_response_code_for_access_denied=http_response_code_for_access_denied
+                )
+                if has_access and role not in service["roles_allowed"]:
+                    # The role is not authorized to access the service
+                    ans.append(f"{role} is not authorized to access {service.name}")
+                elif not has_access and role in service["roles_allowed"]:
+                    # The role is authorized to access the service
+                    ans.append(f"{role} is authorized to access {service.name} but it has no access")
+        return ans
--- testing_auth/views.py
+++ testing_auth/views.py
@@ -1,7 +1,11 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from testing_auth.services.AuthTestingService import AuthTestingService
-
+from rest_framework.decorators import api_view
+import xml.etree.ElementTree as ET
+from rest_framework.response import Response
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
@@ -30,3 +34,26 @@ def verify_username_and_password_strength(request):
             }
         )
     return JsonResponse(response, safe=False)
+
+
+@csrf_exempt
+@api_view(["POST"])
+def check_role_authorization(request):
+        # Get the JSON data from the request body
+        json_data = json.loads(request.body)
+
+        # Extract information from the JSON
+        all_roles = json_data.get('roles')
+        services = json_data.get('services')
+        auth_uri = json_data.get('auth_uri')
+        token_path = json_data.get('token_path_in_response')
+        website_url = json_data.get('website_url')
+        
+        response = AuthTestingService.check_role_authorization(website_url=website_url, all_roles=all_roles,services=services,auth_uri=auth_uri,token_path=token_path)
+        return JsonResponse(response, safe=False)
+        
+        
+
+    
+
+
